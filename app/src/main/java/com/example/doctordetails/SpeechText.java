9742261ed21f196b7e55458f81fd2bc22db9a8bf
hp <chhabraashish123@gmail.com>
2020-02-05 03:09:20 +0530

    Audio to Text

---
+++ app/src/main/java/com/example/doctordetails/SpeechText.java
@@ -0,0 +1,66 @@
+package com.example.doctordetails;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechText extends AppCompatActivity {
+
+    TextView t1;
+    Button b1,b2,b3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_text);
+        t1=findViewById(R.id.text1);
+        b1=findViewById(R.id.again);
+        b2=findViewById(R.id.record);
+        b3=findViewById(R.id.sent);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t1.setText("");
+                speechconvert(view);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              speechconvert(view);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+    public void speechconvert(View v)
+    {
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak Now");
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1&&resultCode==RESULT_OK&&data!=null)
+        {
+            ArrayList<String> list=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            t1.setText(list.get(0).toString());
+        }
+    }
+}
